cf9cefba2a993c8b682c35dee2082a4a60711878
unknown <socgend31@iiht.tech>
2020-07-09 10:17:07 +0530

    Initial commit

---
+++ Assignment1/HandsOnMVCUsingViewModel/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HandsOnMVCUsingViewModel.Models;
+namespace HandsOnMVCUsingViewModel.Controllers
+{
+    public class ProductController : Controller
+    {
+        public static List<Product> list = new List<Product>();
+        public IActionResult Index()
+        {
+            
+            return View(list);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product p)
+        {
+            //adding project data to list
+            list.Add(p);
+            return RedirectToAction("Index"); //redirects to Index Action
+        }
+       
+    }
+}
---
+++ Assignment1/HandsOnMVCUsingViewModel/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HandsOnMVCUsingViewModel.Models
+{
+    public class Product
+    {
+        public string Pid { get; set; }
+        public string Pname { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+
+    }
+}
